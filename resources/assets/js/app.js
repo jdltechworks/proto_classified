070041198a71da6dad62a97e802b7ad05523892a
jdltechworks <jo@bigzeta.com>
2017-06-20 12:37:28 +0800

    Adding updates for react

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react'
 import { render } from 'react-dom'
+import _ from 'lodash'
 
 export default class App extends Component {
+	state = {
+		test: true
+	}
 	render() {
 		return (
-			<div className="app-container">
-				Hello from react
+			<div className="app-container" onClick={(e) => {
+				console.log(this.state.test)
+				this.setState({ test: !this.state.test })
+
+			}}>
+				Hello from react {_.toString(this.state.test)}
 			</div>
 		)
 	}
 }
 
 render(<App />, document.getElementById('app'))
-
